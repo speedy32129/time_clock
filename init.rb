aca8c2fafe0d7a60c6aea6d8b7e7b58554200745
speedy32129 <ironspeeder@hotmail.com>
2019-05-01 16:49:36 -0400

    initial comit after opening commit window

---
+++ init.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+Redmine::Plugin.register :time_clock do
+  name 'Time Clock plugin'
+  author 'Jim McAleer'
+  description 'This a Redmine plugin for user time allocation. This is independent of tasks assiginements. Used to track a users clock in and clock out times.'
+  version '0.0.1'
+  url 'http://github.com/speedy32129/time_clock'
+  #author_url 'http://example.com/about'
+
+  # add link in user account menu
+
+end
